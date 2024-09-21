2daeabc62ca63a30636faf8bd2ec9bbac403fa92
Mohan A  D <125897196+Mohanad014@users.noreply.github.com>
2024-09-21 10:25:11 +0530

    Update attendanceProject.py

--- attendanceProject.py
+++ attendanceProject.py
@@ -15,7 +15,7 @@ classNames = []
 #grab the images list from the ImagesAttendance folder
 myList = os.listdir(path)
 #output-['mohan.jpg,vikram.jpg,lingesh.jpg) 
-print(myList)
+print(myList) 
 #read the image for the file, cl is the name of the image
 for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
